2176490e4742023c13eea485f6fc40ff5457674d
Mayank Gahlot <mayankgahlot08@gmail.com>
2025-08-23 11:27:52 +0530

    Initial Commit

---
+++ Finance app tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance app tracker
+//
+//  Created by MAYANK GAHLOT on 23/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance app tracker/Finance_app_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_app_trackerApp.swift
+//  Finance app tracker
+//
+//  Created by MAYANK GAHLOT on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Finance_app_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
